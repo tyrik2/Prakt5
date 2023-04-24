1a932ce688dde3a1c9af972dc3a9b0f0f33a38ba
Daniel <vfgkc3@gmail.com>
2023-04-24 22:07:24 +0300

    Готовое задание 1

--- Praktik_5/MainWindow.xaml.cs
+++ Praktik_5/MainWindow.xaml.cs
@@ -24,5 +24,69 @@ namespace Praktik_5
         {
             InitializeComponent();
         }
+
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int N = ListBoxData.Items.Count;
+                int maxScore = 0; // максимальное количество правильных ответов
+                bool hasZeroScore = false; // флаг для проверки наличия участников с нулевым количеством правильных ответов
+
+                for (int i = 0; i < N; i++)
+                {
+                    int score = Convert.ToInt32(ListBoxData.Items[i]); // количество правильных ответов текущего участника
+                    if (score > maxScore)
+                    {
+                        maxScore = score; // обновляем максимальное количество правильных ответов
+                    }
+                    if (score == 0)
+                    {
+                        hasZeroScore = true; // устанавливаем флаг, если найден участник с нулевым количеством правильных ответов
+                    }
+                }
+                TextBlockAnswer.Text = $"{maxScore}";
+                if (hasZeroScore)
+                {
+                    TextBlockAnswer2.Text="YES";
+                }
+                else
+                {
+                    TextBlockAnswer2.Text="NO";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
     }
 }
