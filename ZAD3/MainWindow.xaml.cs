25ce47105c9e0c005166c5af36426392fd687270
Daniel <vfgkc3@gmail.com>
2023-04-24 22:46:15 +0300

    Готовое задание 3

---
+++ ZAD3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZAD3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(PriceTextBox.Text, out double price))
+            {
+                for (double weight = 1.2; weight <= 2.0; weight += 0.2)
+                {
+                    double cost = price * weight;
+                    ResultListBox.Items.Add($"{weight} kg: {cost:C}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка.");
+            }
+        }
+    }
+}
